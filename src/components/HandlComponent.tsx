c8bc239b73b5cd5a5872becc3dc5c3afae67d46b
Bhawesh15 <169034549+enclavegaming2020@users.noreply.github.com>
2024-08-16 14:50:39 +0545

    First Deploy

---
+++ src/components/HandlComponent.tsx
@@ -0,0 +1,10 @@
+const HandleComponent = () => {
+  return (
+    <div
+      className="2-5 h-5 wounded-full shadow border 
+    bg-white border-zinc-200 transition hover:bg-primary"
+    ></div>
+  );
+};
+
+export default HandleComponent;
